a299c4bbb9fe392e699890b3e59ec29b811b2419
Swati singh <swatigola01@gmail.com>
2025-08-04 00:02:11 +0530

    add form

--- src/components/Meal/Meals.jsx
+++ src/components/Meal/Meals.jsx
@@ -1,20 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
 import mealsData from "./MealsList"
 import "./Meals.css"
+import { FaPlus } from 'react-icons/fa';
 const Meals = () => {
- console.log(mealsData)
+  let [count, setCount] = useState(1);
+  console.log(mealsData)
+  let handleCount = (id) => {
+    setCount((prevCounts) => ({
+      ...prevCounts,
+      [id]: (prevCounts[id] || 0) + 1,
+    }));
+  }
   return (
-      <div className='meals-container'>
+    <>
+    <div className='meals-container'>
       {mealsData.map((data) => {
-        return <div div key={data.id}>
-        <h3 >{data.name}</h3>
+        return  <React.Fragment key={data.id}>
+ <div  className='description' key={data.id}>
+          <div>
+          <h3 >{data.name}</h3>
           <i>{data.description}</i>
-          <p className='price'><b>${data.price}</b></p>
+            <p className='price'><b>${data.price}</b></p>
+          
+          </div>
+          <div className='amount'>
+            <h3>Amount</h3>
+              <input
+                type="text"
+                disabled
+                className='input'
+               value={count[data.id] ?? 0}
+              />
+            <button onClick={()=>handleCount(data.id)} className='add-btn'>  <FaPlus /> Add</button></div>
+          
+          </div>
           <hr />
-        </div>
+</React.Fragment>
       })}
       
       </div>
+    </>
   )
 }
 
